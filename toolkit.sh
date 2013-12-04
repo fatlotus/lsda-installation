46dd06411c4efbc347e0d0e633d0d99fedc95444
Jeremy Archer <open-source@fatlotus.com>
2013-12-04 14:18:28 -0600

    Ensure that git clone is idempotent.

--- toolkit.sh
+++ toolkit.sh
@@ -83,18 +83,20 @@ sleep 2
 echo -n "Please enter your CNetID: "
 CNETID="$(head -n 1 /dev/tty)"
 
-if [ ! -f lsda-ssh-key.pem ]; then
+if [ ! -f lsda_ssh_key.pem ]; then
   curl --insecure -s -u $CNETID https://lsda.cs.uchicago.edu/generate-ssh-key.cgi > lsda_ssh_key.pem
   ssh-add lsda_ssh_key.pem >> install.log
   rm lsda_ssh_key.pem
 fi
 
-echo
-echo "Next, let's download the starter projects for this assignment. If prompted to"
-echo "\"Are you sure you want to...\", just type yes."
-echo
+if [ ! -f .git ]; then
+  echo
+  echo "Next, let's download the starter projects for this assignment. If prompted to"
+  echo "\"Are you sure you want to...\", just type yes."
+  echo
 
-git clone git@lsda.cs.uchicago.edu:assignment-one .
+  git clone git@lsda.cs.uchicago.edu:assignment-one .
+fi
 
 echo
 echo "Hang tight -- this may take a few minutes."
