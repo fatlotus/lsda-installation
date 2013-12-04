3698c00a9f9d253fa1ddb13d86ceac26aa42482d
Jeremy Archer <open-source@fatlotus.com>
2013-12-04 14:16:54 -0600

    Ensure that keys are kept constant even if invoked multiple times.

--- toolkit.sh
+++ toolkit.sh
@@ -83,8 +83,8 @@ sleep 2
 echo -n "Please enter your CNetID: "
 CNETID="$(head -n 1 /dev/tty)"
 
-if [ ! -f .lsda-ssh-key ]; then
-  curl --insecure -s -u $CNETID https://lsda.cs.uchicago.edu/generate-ssh-key.cgi > lsda_ssh_key.pem >> install.log
+if [ ! -f lsda-ssh-key.pem ]; then
+  curl --insecure -s -u $CNETID https://lsda.cs.uchicago.edu/generate-ssh-key.cgi > lsda_ssh_key.pem
   ssh-add lsda_ssh_key.pem >> install.log
   rm lsda_ssh_key.pem
 fi
