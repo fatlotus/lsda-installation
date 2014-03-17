dba83c65fd97f4d7d4fedd5db611ed8e58992d11
Jeremy Archer <open-source@fatlotus.com>
2014-03-17 12:56:55 -0500

    Fix extra escape bug.

--- toolkit.sh
+++ toolkit.sh
@@ -135,7 +135,7 @@ echo -ne "Adding new LSDA SSH key...\r"
 
 echo "eval \$(ssh-agent) >>/dev/null" >> bin/activate
 echo "ssh-add .lsda_ssh_key.pem 2>&1 | grep -v \"Identity added\"" >> bin/activate
-echo "export GIT_SSH=\"\$\(pwd)/.ssh.sh\"" >> bin/activate
+echo "export GIT_SSH=\"\$(pwd)/.ssh.sh\"" >> bin/activate
 
 . bin/activate
 
